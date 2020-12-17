38c4c4c539a78f891c159e3859fa8e3ee0a18861
Rinat <lich666black@gmail.com>
2020-12-17 16:48:41 +0500

    added webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  target: 'web',
+  resolve: {
+    fallback: {
+      fs: false
+    }
+  },
+  watch: false
+};
